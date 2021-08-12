4ac2d9d161470e48e03eaaa5876662c6c9ba1dd0
Nicholas Rynearson <nrynearson@gmail.com>
2021-08-11 22:33:25 -0400

    creates controller for Works; all CRUD routes for Works tested and functioning

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,43 @@
+class WorksController < ApplicationController
+  before_action :get_work, only: [:show, :update, :destroy]
+  def index
+    works = Work.where(show_id: params[:show_id])
+    render json: works, status: :ok
+  end
+
+  def show
+    render json: @work, status: :ok
+  end
+
+  def create
+    new_work = Work.new(work_params)
+    if new_work.save
+      render json: new_work, status: :ok
+    else
+      render json: new_work.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @work.update(work_params)
+      render json: @work, status: :ok
+    else 
+      render json: @work.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @work.destroy
+    render json: "Deleted", status: :ok
+  end
+
+  private
+
+  def get_work
+    @work = Work.find(params[:id])
+  end
+
+  def work_params
+    params.require(:work).permit(:title, :year, :materials, :show_id, :image_url)
+  end
+end
